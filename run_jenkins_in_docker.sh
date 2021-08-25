da80c58d3fcd17fc12be385a1dc79a7598edcd4d
Gautam Mishra <35479794+ga7uti@users.noreply.github.com>
2021-08-25 16:23:32 +0530

    bash script

---
+++ run_jenkins_in_docker.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+echo "Create network jenkins"
+docker network create jenkins
+
+echo "Running jenkins docker container"
+docker run --name jenkins-docker --rm --detach \
+  --privileged --network jenkins --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 docker:dind --storage-driver overlay2
+
+echo "Building image for jenkins blueocean"
+docker build -t myjenkins .
+
+echo "Running jenkins blueocean container"
+docker run --name jenkins-blueocean --rm --detach \
+  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  --volume "$HOME":/home \
+  myjenkins
+  
